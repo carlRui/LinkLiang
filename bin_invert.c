27d910e358df4fc81443a15fbe01277d296804f3
LinkLiang <1028155894@qq.com>
2023-04-15 00:41:33 +0800

    update need

--- bin_invert.c
+++ bin_invert.c
@@ -10,40 +10,55 @@
 #include <string.h>
 #include <stdbool.h>
 
-void binary_invert(int n);
+void binary_invert(int n,char bit);
 bool arguments_is_digit(char *buff);	
-
+int flag_digit = 0;
+char buff[100];
+char buff2[100];
+int i =0;
 int main(int argc,char *argv[])
 {
 	bool is_digit = false;
+	bool is_bit = false;
 	int digit = 0;
-
-	if(argc>1)
+	int bit = 0;
+	memset(buff,0x0,sizeof(buff));
+	memset(buff2,0x0,sizeof(buff2));
+	if(argc>2)
 	{
 		is_digit = arguments_is_digit(argv[1]);	
+		is_bit = arguments_is_digit(argv[2]);	
 	}else
 	{
-		printf("please input digit\r\n");
+		printf("please input digit and bit\r\n");
 		return 1;
 	}
 	if(is_digit)
 	{
 		digit = atoi(argv[1]);
-		binary_invert(digit);
+		bit = atoi(argv[2]);
+		i =0;
+		binary_invert(digit,bit);
+		printf("%d->%s,%s->%d",digit,buff2,buff,flag_digit);
 		printf("\r\n");
 	}
 	else
 		printf("not is digit\r\n");
 	return 0;
 }
-
-void binary_invert(int n)
+void binary_invert(int n,char bit)
 {
-	printf("%d",n%2);
-	n/=2;
-	if(n==0)
+	if(bit==0)
+	{
 		return;
-	binary_invert(n);
+	}
+	buff[i]=n%2+'0';
+	bit--;
+	i++;
+	binary_invert(n/2,bit);
+	buff2[bit]=n%2+'0';
+	flag_digit|=n%2<<bit;
+
 }
 
 bool arguments_is_digit(char *buff)	
